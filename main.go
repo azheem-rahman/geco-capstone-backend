ca714a6c34ddcee15fdc2dc99280beaf36d6e408
Azheem Rahman <azheem.rahman@gmail.com>
2023-09-18 17:43:02 +0800

    edited postAccount route

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *sql.DB
 
 type user struct {
 	Account_id   int    `json:"account_id"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	Account_Type string `json:"account_type"`
+}
+
+type emailPassword struct {
 	Email        string `json:"email"`
 	Password     string `json:"password"`
 	Account_Type string `json:"account_type"`
@@ -53,11 +60,30 @@ func main() {
 
 	router := gin.Default()
 
+	// Routes related to user account login and creation
+	// router.POST("/login", login)
 	router.GET("/accounts", getAccounts)
 	router.POST("/new-account", postAccount)
+	// router.PATCH("/update-account-password", updateAccountPassword)
+	// router.DELETE("/delete-account", deleteAccount) only Admin
+	// router.PATCH("/update-account-details", updateAccountDetails)
+	// router.GET("/accounts-details", getAccountsDetails)
+	// router.POST("/new-account-details", postAccountDetails)
+
+	// Routes related to orders
+	// router.GET("/orders", getOrders)
+	// router.POST("/new-order", postOrder)
 
 	router.Run("localhost:8080")
 }
+
+// func login(c *gin.Context) {
+
+// }
+
+// func loginDB(loginAccount user) (user, error) {
+
+// }
 
 func getAccounts(c *gin.Context) {
 	accounts, err := getAccountsFromDB()
@@ -92,39 +118,45 @@ func getAccountsFromDB() ([]user, error) {
 }
 
 func postAccount(c *gin.Context) {
-	var newUser user
-
-	c.BindJSON(&newUser)
-
-	result, err := postAccountToDB(newUser)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	c.IndentedJSON(http.StatusOK, result)
-}
-
-func postAccountToDB(newUser user) (int64, error) {
-	// check if email username is already taken -- account already exists
+	// Get email and password for new account from request body
+	var reqBody emailPassword
 	var emailFound bool
 
-	if err := db.QueryRow("SELECT * FROM accounts WHERE email=?", newUser.Email).Scan(&emailFound); err != nil {
-		// email username already taken since found in database
+	// Returns Error HTTP Bad Request 400 if unable to read from request body
+	if c.BindJSON(&reqBody) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Failed to read request body"})
+		return
+	}
+
+	// Check if email already taken
+	if err := db.QueryRow("SELECT * FROM accounts WHERE email=?", reqBody.Email).Scan(&emailFound); err != nil {
+		// if response returns a row means email already exists in database
 		if err != sql.ErrNoRows {
-			fmt.Println("Email taken!")
+			c.JSON(http.StatusBadRequest, gin.H{"Error": "Email taken"})
+			return
 		}
 	}
 
-	result, err := db.Exec("INSERT INTO accounts (email, password, account_type) VALUES (?, ?, ?)", newUser.Email, newUser.Password, newUser.Account_Type)
+	// Hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(reqBody.Password), 10)
 	if err != nil {
-		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Failed to hash password"})
+		return
 	}
 
-	newUserID, err := result.LastInsertId()
+	// Create the user
+	var newAccount emailPassword
+	newAccount.Email = reqBody.Email
+	newAccount.Password = string(hash)
+	newAccount.Account_Type = reqBody.Account_Type
+
+	_, err = db.Exec("INSERT INTO accounts (email, password, account_type) VALUES (?, ?, ?)", newAccount.Email, newAccount.Password, newAccount.Account_Type)
 	if err != nil {
-		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Failed to create user"})
+
+		return
 	}
 
-	return newUserID, nil
-
+	// Respond
+	c.IndentedJSON(http.StatusOK, gin.H{"Success": "Successfully created user"})
 }
